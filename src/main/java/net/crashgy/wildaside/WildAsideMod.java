d58b1ffb8b13390a4c3dde7f209b30ee3b815c49
CrashGyuluka <69863086+CrashGyuluka@users.noreply.github.com>
2022-04-30 19:53:47 +0200

    Added testitems and testblock

--- src/main/java/net/crashgy/wildaside/WildAsideMod.java
+++ src/main/java/net/crashgy/wildaside/WildAsideMod.java
@@ -1,10 +1,13 @@
 package net.crashgy.wildaside;
 
 import com.mojang.logging.LogUtils;
+import net.crashgy.wildaside.block.ModBlocks;
+import net.crashgy.wildaside.item.ModItems;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.InterModComms;
 import net.minecraftforge.fml.common.Mod;
@@ -27,9 +30,13 @@ public class WildAsideMod
 
     public WildAsideMod()
     {
-        // Register the setup method for modloading
-        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
-        // Register ourselves for server and other game events we are interested in
+        IEventBus eventBus = FMLJavaModLoadingContext.get().getModEventBus();
+
+        ModItems.register(eventBus);
+        ModBlocks.register(eventBus);
+
+        eventBus.addListener(this::setup);
+
         MinecraftForge.EVENT_BUS.register(this);
     }
 
---
+++ src/main/java/net/crashgy/wildaside/item/ModItems.java
@@ -0,0 +1,25 @@
+package net.crashgy.wildaside.item;
+
+import net.crashgy.wildaside.WildAsideMod;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.Item;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+
+public class ModItems {
+    public static final DeferredRegister<Item> ITEMS =
+            DeferredRegister.create(ForgeRegistries.ITEMS, WildAsideMod.MOD_ID);
+
+    public static final RegistryObject<Item> TESTITEM = ITEMS.register("testitem",
+            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_MISC)));
+
+    public static final RegistryObject<Item> TESTITEM2 = ITEMS.register("testitem2",
+            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_MISC)));
+
+    public static void register(IEventBus eventBus)
+    {
+        ITEMS.register(eventBus);
+    }
+}
