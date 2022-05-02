a5f9927d85752ba58913be9af0fda11a6cc843c3
CrashGyuluka <69863086+CrashGyuluka@users.noreply.github.com>
2022-05-02 17:08:37 +0200

    Added even more blocks and properties! (missing texture error tho)

--- src/main/java/net/crashgy/wildaside/block/ModBlocks.java
+++ src/main/java/net/crashgy/wildaside/block/ModBlocks.java
@@ -8,6 +8,7 @@ import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.RotatedPillarBlock;
+import net.minecraft.world.level.block.SoundType;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.material.Material;
 import net.minecraftforge.eventbus.api.IEventBus;
@@ -16,11 +17,13 @@ import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
 
 import java.util.function.Supplier;
+import java.util.function.ToIntFunction;
 
 public class ModBlocks {
     public static final DeferredRegister<Block> BLOCKS =
             DeferredRegister.create(ForgeRegistries.BLOCKS, WildAsideMod.MOD_ID);
 
+    //TEST BLOCKS!
     public static final RegistryObject<Block> TESTBLOCK = registerBlock("testblock",
             () -> new Block(BlockBehaviour.Properties.of(Material.WOOD)
                     .strength(9f)
@@ -31,10 +34,32 @@ public class ModBlocks {
                     .strength(9f)
                     .requiresCorrectToolForDrops()), ModCreativeModeTab.WILDASIDE_TAB);
 
-    public static final RegistryObject<Block> SUBSTILIUM_STEAM = registerBlock("substilium_stem",
+
+    //SUBSTILIUM & VIRBION (Vibrion Vines, Vibrion Growths and Sporeholders are not implemented)
+    public static final RegistryObject<Block> SUBSTILIUM_STEM = registerBlock("substilium_stem",
             () -> new RotatedPillarBlock(BlockBehaviour.Properties.of(Material.WOOD)
-                    .strength(9f)
+                    .strength(3f)
                     .requiresCorrectToolForDrops()), ModCreativeModeTab.WILDASIDE_TAB);
+
+    public static final RegistryObject<Block> SUBSTILIUM_SHROOM = registerBlock("substilium_shroom",
+            () -> new Block(BlockBehaviour.Properties.of(Material.WOOD)
+                    .strength(0.3f)
+                    .requiresCorrectToolForDrops()), ModCreativeModeTab.WILDASIDE_TAB);
+
+    public static final RegistryObject<Block> VIBRION_BLOCK = registerBlock("vibrion_block",
+            () -> new Block(BlockBehaviour.Properties.of(Material.VEGETABLE)
+                    .strength(2.2f)
+                    .sound(SoundType.SHROOMLIGHT)
+                    .lightLevel(s -> 6)
+                    .explosionResistance(1f)
+                    //.emissiveRendering((a, b, c) -> true)
+                    .requiresCorrectToolForDrops()), ModCreativeModeTab.WILDASIDE_TAB);
+
+    public static final RegistryObject<Block> SUBSTILIUM_SOIL = registerBlock("substilium_soil",
+            () -> new RotatedPillarBlock(BlockBehaviour.Properties.of(Material.WOOD)
+                    .strength(0.6f)
+                    .requiresCorrectToolForDrops()), ModCreativeModeTab.WILDASIDE_TAB);
+
 
 
     private static <T extends Block> RegistryObject<T> registerBlock(String name, Supplier<T> block, CreativeModeTab tab)
