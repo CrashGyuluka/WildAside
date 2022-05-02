68fa4aef76789c311d61e43e54125b85eb27622d
CrashGyuluka <69863086+CrashGyuluka@users.noreply.github.com>
2022-05-02 20:44:17 +0200

    Some code organization (and Discord webhook test :D)

--- src/main/java/net/crashgy/wildaside/block/ModBlocks.java
+++ src/main/java/net/crashgy/wildaside/block/ModBlocks.java
@@ -23,6 +23,7 @@ public class ModBlocks {
     public static final DeferredRegister<Block> BLOCKS =
             DeferredRegister.create(ForgeRegistries.BLOCKS, WildAsideMod.MOD_ID);
 
+
     //TEST BLOCKS!
     public static final RegistryObject<Block> TESTBLOCK = registerBlock("testblock",
             () -> new Block(BlockBehaviour.Properties.of(Material.WOOD)
@@ -46,20 +47,19 @@ public class ModBlocks {
 //                    .strength(0.3f)
 //                    .requiresCorrectToolForDrops()), ModCreativeModeTab.WILDASIDE_TAB);
 
-    public static final RegistryObject<Block> VIBRION_BLOCK = registerBlock("vibrion_block",
-            () -> new Block(BlockBehaviour.Properties.of(Material.VEGETABLE)
-                    .strength(2.2f)
-                    .sound(SoundType.SHROOMLIGHT)
-                    .lightLevel(s -> 6)
-                    .explosionResistance(1f)
-                    //.emissiveRendering((a, b, c) -> true)
-                    .requiresCorrectToolForDrops()), ModCreativeModeTab.WILDASIDE_TAB);
-
 //    public static final RegistryObject<Block> SUBSTILIUM_SOIL = registerBlock("substilium_soil",
 //            () -> new RotatedPillarBlock(BlockBehaviour.Properties.of(Material.WOOD)
 //                    .strength(0.6f)
 //                    .requiresCorrectToolForDrops()), ModCreativeModeTab.WILDASIDE_TAB);
 
+    public static final RegistryObject<Block> VIBRION_BLOCK = registerBlock("vibrion_block",
+            () -> new Block(BlockBehaviour.Properties.of(Material.VEGETABLE)
+                    .strength(2.2f)
+                    .explosionResistance(1f)
+                    .lightLevel(s -> 6)
+                    .sound(SoundType.SHROOMLIGHT)
+                    //.emissiveRendering((a, b, c) -> true)
+                    .requiresCorrectToolForDrops()), ModCreativeModeTab.WILDASIDE_TAB);
 
 
     private static <T extends Block> RegistryObject<T> registerBlock(String name, Supplier<T> block, CreativeModeTab tab)
