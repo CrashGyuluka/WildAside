0b326de8bd80130f08a413dd5577fac275f9a5c7
Toma400 <thomasso_calgori@o2.pl>
2022-05-22 21:52:30 +0200

    Loot table datagen updated for ores (visit datagen.Helpers.modOres to include non-OreType blocks) and pushed testblock loot table into datagen

--- src/main/java/net/crashgy/wildaside/datagen/Helpers.java
+++ src/main/java/net/crashgy/wildaside/datagen/Helpers.java
@@ -4,6 +4,8 @@ import net.crashgy.wildaside.WildAsideMod;
 import net.crashgy.wildaside.block.ModBlocks;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.OreBlock;
+import net.minecraftforge.registries.RegistryObject;
 
 import java.util.ArrayList;
 
@@ -19,6 +21,16 @@ public class Helpers {
         //manual_jsons_list.add(ModBlocks.SUBSTILIUM_SHROOM.get());
         //manual_jsons_list.add(ModBlocks.SUBSTILIUM_SOIL.get());
         return (manual_jsons_list.contains(block));
+    }
+    public static Boolean modOres(Block block) {
+        ArrayList<Block> ores_list = new ArrayList<>();
+        for (RegistryObject<Block> block_iterated : ModBlocks.BLOCKS.getEntries()) {
+            if (block_iterated.get() instanceof OreBlock) {
+                ores_list.add(block_iterated.get());
+            }
+        }
+        ores_list.add(ModBlocks.TESTBLOCK.get());
+        return (ores_list.contains(block));
     }
 
     //ITEM GENERATION
--- src/main/java/net/crashgy/wildaside/datagen/LootTablesGen.java
+++ src/main/java/net/crashgy/wildaside/datagen/LootTablesGen.java
@@ -4,11 +4,15 @@ import com.google.common.collect.ImmutableList;
 import com.mojang.datafixers.util.Pair;
 import net.crashgy.wildaside.WildAsideMod;
 import net.crashgy.wildaside.block.ModBlocks;
+import net.crashgy.wildaside.item.ModItems;
 import net.minecraft.data.DataGenerator;
 import net.minecraft.data.loot.BlockLoot;
 import net.minecraft.data.loot.LootTableProvider;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.Items;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.OreBlock;
 import net.minecraft.world.level.storage.loot.LootTable;
 import net.minecraft.world.level.storage.loot.ValidationContext;
 import net.minecraft.world.level.storage.loot.parameters.LootContextParamSet;
@@ -60,11 +64,37 @@ public class LootTablesGen {
             for (RegistryObject<Block> block : blocks) {
 
                 //-------------------------------------------------
+                // ORE DROPS
+                //-------------------------------------------------
+                if (Helpers.modOres(block.get())) {
+                    String namingConvention = block.get().getRegistryName().getPath();
+                    Item itemProvided = block.get().asItem();
+                    // -------------------------------------------------
+                    // CUSTOM ORE DROPS
+                    // If custom drop is not chosen, block will get
+                    // silk-touched anyway (like pre-1.17 iron ore)
+                    // -------------------------------------------------
+                    if (namingConvention.contains("testblock")) {
+                        itemProvided = ModItems.TESTITEM.get();
+                    }
+                    //-------------------------------------------------
+                    oresGen(block.get(), itemProvided);
+                } else {
+                //-------------------------------------------------
                 // REGULAR BLOCKS (DROP ITSELF)
                 //-------------------------------------------------
-                dropSelf(block.get());
+                    dropSelf(block.get());
+                }
             }
         }
+
+        //--------------------------------------------------------------------------------------------------------
+        // GENERATION ACTORS
+        // Used to simplify some unnecessary code spaghetti in methods above
+        //--------------------------------------------------------------------------------------------------------
+        public void oresGen(Block blockProvided, Item itemProvided) {
+            this.add(blockProvided, (sTouch) -> {
+                return createOreDrop(blockProvided, itemProvided);});}
 
         @Override
         protected @NotNull Iterable<Block> getKnownBlocks() {
