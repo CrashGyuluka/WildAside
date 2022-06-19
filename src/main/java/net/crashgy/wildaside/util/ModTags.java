18de8239b438af0b0518ba791ede717d58c8bf86
CrashGyuluka <69863086+CrashGyuluka@users.noreply.github.com>
2022-06-19 12:48:32 +0200

    Added Aqu to CREDITS.txt and mods.toml, and added new tags

--- src/main/java/net/crashgy/wildaside/util/ModTags.java
+++ src/main/java/net/crashgy/wildaside/util/ModTags.java
@@ -12,6 +12,8 @@ import net.minecraftforge.common.Tags;
 public class ModTags {
     public static class Blocks {
         public static final TagKey<Block> ALL_WILDASIDE_BLOCKS = tag("all_wildaside_blocks");
+        public static final TagKey<Block> VIBRION_BLOCKS = tag("vibrion_blocks");
+        public static final TagKey<Block> SUBSTILIUM_BLOCKS = tag("substilium_blocks");
 
         private static TagKey<Block> tag(String name) {
             return BlockTags.create(new ResourceLocation(WildAsideMod.MOD_ID, name));
@@ -24,6 +26,7 @@ public class ModTags {
     }
 
     public static class Items {
+        public static final TagKey<Item> ALL_WILDASIDE_ITEMS = tag("all_wildaside_items");
 
         private static TagKey<Item> tag(String name) {
             return ItemTags.create(new ResourceLocation(WildAsideMod.MOD_ID, name));
