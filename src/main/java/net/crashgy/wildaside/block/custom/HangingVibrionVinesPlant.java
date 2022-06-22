b97e118f35a2c36131980bc444f443eb1f0b40ac
CrashGyuluka <69863086+CrashGyuluka@users.noreply.github.com>
2022-06-22 13:20:33 +0200

    Hanging Vibrion Vines Plant now drops Hanging Vine Plant too

--- src/main/java/net/crashgy/wildaside/block/custom/HangingVibrionVinesPlant.java
+++ src/main/java/net/crashgy/wildaside/block/custom/HangingVibrionVinesPlant.java
@@ -1,11 +1,15 @@
 package net.crashgy.wildaside.block.custom;
 
 import net.crashgy.wildaside.block.ModBlocks;
-import net.minecraft.core.BlockPos;
-import net.minecraft.world.level.BlockGetter;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.block.GrowingPlantHeadBlock;
 import net.minecraft.world.level.block.WeepingVinesPlantBlock;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.storage.loot.LootContext;
+import net.minecraftforge.common.extensions.IForgeBlockState;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class HangingVibrionVinesPlant extends WeepingVinesPlantBlock {
 
@@ -16,5 +20,12 @@ public class HangingVibrionVinesPlant extends WeepingVinesPlantBlock {
     @Override
     protected GrowingPlantHeadBlock getHeadBlock() {
         return (GrowingPlantHeadBlock)ModBlocks.HANGING_VIBRION_VINES.get();
+    }
+
+    @Override
+    public List<ItemStack> getDrops(BlockState pState, LootContext.Builder pBuilder) {
+        List<ItemStack> out = new ArrayList<>();
+        final IForgeBlockState state = ModBlocks.HANGING_VIBRION_VINES.get().defaultBlockState();
+        return out;
     }
 }
